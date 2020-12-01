8412e15c68c11c2ce2ef630c0f69e080aa2d1480
Guillermo Brachetta <brachetta@me.com>
2020-12-01 22:57:39 +0100

    sett

--- eliskitchen/settings.py
+++ eliskitchen/settings.py
@@ -91,7 +91,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                # "django.template.context_processors.media",  # Media
+                "django.template.context_processors.media",  # Media
             ],
             "builtins": [
                 "crispy_forms.templatetags.crispy_forms_tags",
